8aa009b77b745839928172544a9da66407eeae84
jonharris37 <jonharris37@users.noreply.github.com>
2025-09-06 13:33:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,5 @@ st.title('Machine Learning App')
 
 st.write('My first ML learning app!')
 
-# add a checkbox ( user interaction)
-checkBox = st.checkbox("Display text")
-if True checkBox:
 
-st.text("Checkbox on!")
 
